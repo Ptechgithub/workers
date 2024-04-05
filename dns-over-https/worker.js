2150a41ba2a20670aefa55f4e5f2d90e3698e110
Peyman <118217228+Ptechgithub@users.noreply.github.com>
2024-04-05 23:34:08 +0330

    update worker.js

--- dns-over-https/worker.js
+++ dns-over-https/worker.js
@@ -1,1 +1,40 @@
+// SPDX-License-Identifier: 0BSD
 
+const doh = 'https://security.cloudflare-dns.com/dns-query';
+const contype = 'application/dns-message';
+const jstontype = 'application/dns-json';
+const r404 = new Response(null, { status: 404 });
+
+export default {
+    async fetch(request) {
+        return handleRequest(request);
+    },
+};
+
+async function handleRequest(request) {
+    let response = r404;
+    const { method, headers, url } = request;
+    const searchParams = new URL(url).searchParams;
+    
+    if (method === 'GET' && searchParams.has('dns')) {
+        const dohUrl = new URL(doh);
+        dohUrl.searchParams.append('dns', searchParams.get('dns'));
+        response = await fetch(dohUrl, { method: 'GET', headers: { 'Accept': contype } });
+    } else if (method === 'POST' && headers.get('content-type') === contype) {
+        const requestBody = await request.arrayBuffer();
+        response = await fetch(doh, {
+            method: 'POST',
+            headers: {
+                'Accept': contype,
+                'Content-Type': contype,
+            },
+            body: requestBody,
+        });
+    } else if (method === 'GET' && headers.get('Accept') === jstontype) {
+        const dohJsonUrl = new URL(doh);
+        dohJsonUrl.search = url.search;
+        response = await fetch(dohJsonUrl, { method: 'GET', headers: { 'Accept': jstontype } });
+    }
+    
+    return response;
+}
